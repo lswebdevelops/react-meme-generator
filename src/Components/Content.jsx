26db569766e042edd2b8adbaa4511eaf82d2766f
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-04-25 20:43:17 -0300

    added styling for desktop and mobile

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -4,7 +4,21 @@ import React from 'react';
 
 function Content(){
     return(
-        <h1>content</h1>
+        <div className='content-container'>
+            <div className='input-container'>
+            <input className='input-left' placeholder='shut up'></input>
+            <input className='input-right' placeholder='and take my money'></input>
+            </div>
+           
+                <button className='btn-meme'>Get a new meme image 
+                <img className='img-troll-btn'
+                 src={require('../images/image-trollmemehat.png')}
+                />
+                </button>
+                <img className='img-content' src={require(`../images/image-moneyback.png`)} alt="money" />
+            
+
+        </div>
     )
 }
 
