6015363d3f0a8f5c10d29aa5db360ef5eb780f83
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-04-25 21:14:13 -0300

    darken the image as mouse is over/out

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,30 +1,47 @@
 import '../Styles/Content.css';
 import React from 'react';
 
+function Content() {
+  function handleClick() {
+    console.log("I was clicked");
+  }
 
-function Content(){
-    function handleClick(){
-        console.log("i was clicked");
-    }
-    return(
-        <div className='content-container'>
-            <div className='input-container'>
-            <input className='input-left' placeholder='shut up'></input>
-            <input className='input-right' placeholder='and take my money'></input>
-            </div>
-           
-                <button 
-                onClick={handleClick}
-                className='btn-meme'>Get a new meme image 
-                <img className='img-troll-btn'
-                 src={require('../images/image-trollmemehat.png')}
-                />
-                </button>
-                <img className='img-content' src={require(`../images/image-moneyback.png`)} alt="money" />
-            
+  function darkenImage(event) {
+    event.target.classList.add('img-content-dark');
+  }
 
-        </div>
-    )
+  function lightenImage(event) {
+    event.target.classList.remove('img-content-dark');
+  }
+
+  return (
+    <div className='content-container'>
+      <div className='input-container'>
+        <input className='input-left' placeholder='shut up'></input>
+        <input className='input-right' placeholder='and take my money'></input>
+      </div>
+
+      <button 
+        onClick={handleClick}
+        className='btn-meme'
+      >
+        Get a new meme image 
+        <img 
+          className='img-troll-btn'
+          src={require('../images/image-trollmemehat.png')}
+          alt='troll'
+        />
+      </button>
+
+      <img  
+        onMouseOver={darkenImage} 
+        onMouseOut={lightenImage}
+        className='img-content' 
+        src={require(`../images/image-moneyback.png`)} 
+        alt="money" 
+      />
+    </div>
+  );
 }
 
 export default Content;
