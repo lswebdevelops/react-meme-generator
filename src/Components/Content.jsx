044b40f91e9b8f65dd46ab2c9e4b9a0c3833560b
lswebdevelops <sardanhaluciano9690@gmail.com>
2023-05-10 22:26:20 -0300

    adding text to the image using useState fetching data from api

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,71 +1,80 @@
-import '../Styles/Content.css';
-import React from 'react';
-import memesData from '../Data/memesData';
+import "../Styles/Content.css";
+import React from "react";
+import memesData from "../Data/memesData";
 
 function Content() {
- 
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/3si4.jpg",
+  });
 
-const [meme, setMeme]  =React.useState({
-  topText:"",
-  bottomText:"",
-  randomImage: require('../images/image-take_money.png')
-})
+  const [allMeme, setAllMeme] = React.useState(memesData);
 
-const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+  React.useEffect(()=>{
 
+    fetch("http://api.imgflip.com/get_memes")
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }, []);
 
-function getMemeImage(){
-  const memesArray = allMemeImages.data.memes;
-  const randomNumber = Math.floor(Math.random() * memesArray.length);
-  const url = memesArray[randomNumber].url;
-  setMeme(prevMeme => ({
-    ...prevMeme,
-    randomImage: url
+  function getMemeImage() {
+    const memesArray = allMeme.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
+  }
+
+  function changeText(event){
+  const {name, value } = event.target;
+  setMeme(prevText =>({
+    ...prevText,
+    [name]: value,
   }))
-}
+  }
 
-
-
-
-
-
-
-
-
-function toggleImage(event){
-  event.target.classList.toggle('img-content-dark')
-}
-
-
-
+  function toggleImage(event) {
+    event.target.classList.toggle("img-content-dark");
+  }
 
   return (
-    <div className='content-container'>
-      <div className='input-container'>
-        <input className='input-left' ></input>
-        <input className='input-right'></input>
+    <div className="content-container">
+      <div className="input-container">
+        <input
+        name="topText"
+        value={meme.topText}
+        onChange={changeText}
+        className="input-left"></input>
+        <input
+        name="bottomText"
+        value={meme.bottomText}
+        onChange={changeText}
+        className="input-right"></input>
       </div>
 
-      <button 
-        onClick={getMemeImage}
-        
-        className='btn-meme'
-      >
-        Get a new meme image 
-        <img 
-          className='img-troll-btn'
-          src={require('../images/image-trollmemehat.png')}
-          alt='troll'
+      <button onClick={getMemeImage} className="btn-meme">
+        Get a new meme image
+        <img
+          className="img-troll-btn"
+          src={require("../images/image-trollmemehat.png")}
+          alt="troll"
         />
       </button>
-
-      <img       
+      <div className="image_meme_container">
+      <h1 className="image_meme_first_phrase" >{meme.topText}</h1>
+      <h1 className="image_meme_second_phrase">{meme.bottomText}</h1>
+      <img
         src={meme.randomImage}
         onMouseEnter={toggleImage}
         onMouseLeave={toggleImage}
-        className='img-content'  alt=" just a meme"
-              
+        className="img-content"
+        alt=" just a meme"
       />
+
+      </div>
      
     </div>
   );
